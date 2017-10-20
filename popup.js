199f8686110fab567761a871e1ae0700bbae1457
Panagiotis Nikoloutsopoulos <nikoloups@gmail.com>
2017-10-20 23:02:12 +0300

    Remove redundant stuff

--- popup.js
+++ popup.js
@@ -23,9 +23,6 @@ function getCurrentTabUrl(callback) {
     // A window can only have one active tab at a time, so the array consists of
     // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
     var title = tab.title;
 
@@ -39,14 +36,6 @@ function getCurrentTabUrl(callback) {
   });
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url,title) => {
     //For now we assume it's a LinkedIn page
@@ -65,22 +54,5 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById("main").removeChild(lgif);
       document.getElementById("main").innerHTML = "<p>Sorry, this service is not supported yet</p>";
     }
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-/*
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });*/
   });
 });
