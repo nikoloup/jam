7aaa4621ff66d448f0c71169211c278032ca02d7
Panagiotis Nikoloutsopoulos <nikoloups@gmail.com>
2017-10-25 20:20:20 +0300

    Background page and Context option added

--- popup.js
+++ popup.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 /**
  * Get the current URL.
  *
@@ -9,33 +5,32 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
     var url = tab.url;
     var title = tab.title;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url,title);
   });
 }
 
+//Create Context Menu for main view
+chrome.contextMenus.create({
+      title: "first",
+      contexts: ["browser_action"],
+      onclick: function() {
+        alert('first');
+      }
+});
+
+//chrome.browserAction.onClicked.addListener(() => {
 document.addEventListener('DOMContentLoaded', () => {
   getCurrentTabUrl((url,title) => {
     //For now we assume it's a LinkedIn page
